2f32d4019a79df895c5de8b28abd9fd96a095d91
Nick Heyland <nheyland@gmail.com>
2021-07-14 23:56:08 -0400

    fix

--- ap.py
+++ ap.py
@@ -72,3 +72,5 @@ class metarmap:
     def testAmount(self, num):
         for i in range(0, num, 1):
             self.np[i] = (0, 0, 255)
+        for i in range(num, self.num_leds, 1):
+            self.np[i] = (0, 255, 0)
